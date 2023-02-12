b994f07cc6f3bac8c9954451992d1822de2672fb
Goldaxe <pierreleveque.pro@gmail.com>
2023-02-12 12:02:06 +0100

    Init commit 📦

---
+++ src/listeningSocketAsPromise.ts
@@ -0,0 +1,33 @@
+import { Socket } from "socket.io-client";
+
+/**
+ * Allows you to connect to a relatively generic event and make a promise out of it. It is possible to specific
+ * a validator to filter the result, as soon as a result matches the promise is resolved with the object received and ready
+ * in use. In case more than timeoutInMillis ms have elapsed, reject the promise.
+ */
+export default function listeningSocketAsPromise<T>(
+	socket: Socket,
+	eventName: string,
+	validator?: (value: T) => boolean,
+	timeoutInMillis?: number
+) {
+	const defaultTimeoutInMillis = 60000; // 60s
+
+	return new Promise<T>((resolve, reject) => {
+		const eventTimeout = setTimeout(() => {
+			reject("Timeout event");
+			socket.off(eventName, listener);
+		}, timeoutInMillis !== undefined ? timeoutInMillis : defaultTimeoutInMillis);
+
+		function listener(eventObj: T)  {
+			if (!validator?.(eventObj)) {
+				return;
+			}
+			resolve(eventObj);
+			clearTimeout(eventTimeout);
+			socket.off(eventName, listener);
+		}
+
+		socket.on(eventName, listener);
+	});
+}
