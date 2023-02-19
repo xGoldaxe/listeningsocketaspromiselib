37ce78fe163ccd9effbff1a5e9160547eb8e321f
Goldaxe <pierreleveque.pro@gmail.com>
2023-02-19 17:33:54 +0100

    fix compilation

--- index.d.ts
+++
@@ -1,8 +0,0 @@
-import { Socket } from "socket.io-client";
-
-export function listeningSocketAsPromise<T>(
-    socket: Socket,
-    eventName: string,
-    validator?: (value: T) => boolean,
-    timeoutInMillis?: number
-);
